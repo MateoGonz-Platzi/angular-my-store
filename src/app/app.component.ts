6c0e08977762b0607eca806b51483c43671e834e
TeoGonz <Matt.Grisa@Gmail.com>
2022-07-18 21:18:23 -0500

    clase 5: Componente Producto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,35 @@
 import { Component } from '@angular/core';
 
+import { Product } from './models/product.model'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public title = 'angular-my-store';
+  public imgFromFather: string = ''; //https://www.w3schools.com/howto/img_avatar.png
+  public ProductArray: Product[] = [
+    {
+      id: '1',
+      name: 'Automobil de juguete',
+      price: 100,
+      image: '../assets/img/image1.jpeg'
+    },
+    {
+      id: '2',
+      name: 'Muñeca de trapo',
+      price: 180,
+      image: '../assets/img/image2.jpeg'
+    },
+    {
+      id: '3',
+      name: 'Pelota de futbol',
+      price: 120,
+      image: '../assets/img/image3.jpeg'
+    }
+  ];
 
-  imgFromFather: string = ''; //https://www.w3schools.com/howto/img_avatar.png
-
-  title = 'angular-my-store';
 
   onLoadedImg(img: string) {
     console.log(img);

3040f40406cae758cd12f62b46fa4b6608b44d71
TeoGonz <Matt.Grisa@Gmail.com>
2022-07-24 14:17:06 -0500

    Clase 10: Implementando SideMenu

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  activeMenu:boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleMenu() {
+    this.activeMenu = !this.activeMenu;
+  }
 }
